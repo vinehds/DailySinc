299519f2e5833ec21bfe1861a94b484d04b7a982
Vinehds <viniciushdalaqua@gmail.com>
2025-08-13 19:34:19 -0300

    project created

---
+++ src/test/java/com/vinehds/dailysinc/DailysincApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinehds.dailysinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailysincApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
